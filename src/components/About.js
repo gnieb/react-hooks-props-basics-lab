dd879fffb7d7dda3603fcb426e764c639000aa41
Grace Nieboer <grace.nieboer@outlook.com>
2023-02-21 12:56:45 -0500

    components rendered using props

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
-  return (
+function About(props) {
+  const isBio = props.bio
+  if (isBio) {
+    return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+       {
+       <p>{props.bio}</p>
+      }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
-  );
+    )}
+      return (
+        <div id="about">
+      <h2>About Me</h2>
+       {/* <p>{props.bio}</p> */}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links />
+    </div>
+      )
 }
 
 export default About;
